34abba1ca62cd4a264584a53fb4d743d5f7ccf34
Ajay malav <malava093@gmail.com>
2024-11-26 14:16:45 +0530

    gender.py

--- gender.py
+++ gender.py
@@ -6,7 +6,7 @@ def Gender():
     import seaborn as sns
     st.write("---")
     st.header("Diwali Sales Analysis")
-    st.sub_header("Gender")
+    st.subheader("Gender")
     st.code("df = pd.read_csv('Diwali Sales Data.csv', encoding= 'unicode_escape')")
     df = pd.read_csv('Diwali Sales Data.csv', encoding= 'unicode_escape')
     st.code("""ax = sns.countplot(x = 'Gender',data = df)
