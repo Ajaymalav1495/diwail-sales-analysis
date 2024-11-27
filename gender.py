a1f64137d0cd45e9031a70babc0bec467b6609fb
Ajay malav <malava093@gmail.com>
2024-11-27 10:01:00 +0530

    gender.py

--- gender.py
+++ gender.py
@@ -1,36 +1,40 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
 def Gender():
-    import streamlit as st
-    import numpy as np 
-    import pandas as pd 
-    import matplotlib.pyplot as plt 
-    import seaborn as sns
     st.write("---")
     st.header("Diwali Sales Analysis")
     st.subheader("Gender")
-    st.code("df = pd.read_csv('Diwali Sales Data.csv', encoding= 'unicode_escape')")
-    df = pd.read_csv('Diwali Sales Data.csv', encoding= 'unicode_escape')
-    st.code("""ax = sns.countplot(x = 'Gender',data = df)
-
+    df = pd.read_csv('Diwali Sales Data.csv', encoding='unicode_escape')
+    
+    st.code("""sns.set(rc={'figure.figsize':(20,5)})
+ax = sns.countplot(x = 'Gender', data = df)
 for bars in ax.containers:
-    ax.bar_label(bars)""")
-    ax = sns.countplot(x = 'Gender',data = df)
-
+    ax.bar_label(bars)
+st.pyplot(plt)""")
+    
+    sns.set(rc={'figure.figsize':(20,5)})
+    ax = sns.countplot(x = 'Gender', data = df)
     for bars in ax.containers:
         ax.bar_label(bars)
     st.pyplot(plt)
-
+    
     st.write("---")
-
-    st.code("""# plotting a bar chart for gender vs total amount
-
-sales_gen = df.groupby(['Gender'], as_index=False)['Amount'].sum().sort_values(by='Amount', ascending=False)
-
-sns.barplot(x = 'Gender',y= 'Amount' ,data = sales_gen)""")
-
-    # plotting a bar chart for gender vs total amount
-
+    
+    st.code("""sales_gen = df.groupby(['Gender'], as_index=False)['Amount'].sum().sort_values(by='Amount', ascending=False)
+plt.figure(figsize=(10, 7))
+plt.pie(sales_gen['Amount'], labels=sales_gen['Gender'], autopct='%1.1f%%')
+plt.title('Gender vs Total Amount')
+st.pyplot(plt)""")
+    
     sales_gen = df.groupby(['Gender'], as_index=False)['Amount'].sum().sort_values(by='Amount', ascending=False)
 
-    sns.barplot(x = 'Gender',y= 'Amount' ,data = sales_gen)
-
+    plt.figure(figsize=(10, 7))
+    plt.pie(sales_gen['Amount'], labels=sales_gen['Gender'], autopct='%1.1f%%')
+    plt.title('Gender vs Total Amount')
     st.pyplot(plt)
+
+Gender()
